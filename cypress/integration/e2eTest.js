82febce9b3dc4093b3ca3219019fd7cdbce7d45f
iemeli <eemeli@paalimaki.fi>
2022-03-15 15:23:33 +0200

    Update build to enable e2e tests

--- cypress/integration/e2eTest.js
+++ cypress/integration/e2eTest.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
-describe('Note app', function () {
+describe('Pokedex app', function () {
   it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
-    cy.contains('bulbasaur')
+    cy.contains('ivysaur')
   })
 })
